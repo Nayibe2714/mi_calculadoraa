8c6c11ca813653b16e0c4a10bdbaef2ea3c3e50f
Nayibe2714 <nayibevelandiaq@gmail.com>
2025-07-25 23:40:42 -0500

    Agregada función elevar al cubo

--- Calculadora.py
+++ Calculadora.py
@@ -12,24 +12,35 @@ def dividir(a, b):
         return "Error: No se puede dividir entre 0"
     return a / b
 
+def elevar_al_cubo(a):
+    return a ** 3
+
 print("Calculadora en Python")
 print("Opciones:")
 print("1. Sumar")
 print("2. Restar")
 print("3. Multiplicar")
 print("4. Dividir")
+print("5. Elevar al cubo") 
 
-opcion = input("Elige una opción (1/2/3/4): ")
-num1 = float(input("Ingresa el primer número: "))
-num2 = float(input("Ingresa el segundo número: "))
+opcion = input("Elige una opción (1/2/3/4/5): ")
 
-if opcion == '1':
-    print("Resultado:", sumar(num1, num2))
-elif opcion == '2':
-    print("Resultado:", restar(num1, num2))
-elif opcion == '3':
-    print("Resultado:", multiplicar(num1, num2))
-elif opcion == '4':
-    print("Resultado:", dividir(num1, num2))
+if opcion == '5':
+    num = float(input("Ingresa el número que deseas elevar al cubo: "))
+    print("Resultado:", elevar_al_cubo(num))
+
 else:
-    print("Opción no válida")
+    num1 = float(input("Ingresa el primer número: "))
+    num2 = float(input("Ingresa el segundo número: "))    
+
+
+    if opcion == '1':
+     print("Resultado:", sumar(num1, num2))
+    elif opcion == '2':
+     print("Resultado:", restar(num1, num2))
+    elif opcion == '3':
+     print("Resultado:", multiplicar(num1, num2))
+    elif opcion == '4':
+     print("Resultado:", dividir(num1, num2))
+    else:
+     print("Opción inválida.")
